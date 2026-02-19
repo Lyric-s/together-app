0afbfe41e34df0b3e2fd6215be78d95ae39a9678
Mromellete <mohamedam555555555@gmail.com>
2026-02-19 22:18:15 +0100

    test: add geolocation utils tests + ci

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,23 @@
 module.exports = {
     preset: "jest-expo",
     testMatch: ["**/__tests__/**/*.test.ts?(x)"],
+    setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+
     collectCoverage: true,
-    collectCoverageFrom: [
-        "utils/**/*.{ts,tsx}",
-        "!**/*.d.ts",
-        "!**/node_modules/**",
-    ],
     coverageDirectory: "coverage",
     coverageReporters: ["text", "lcov", "json"],
-    setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"],
+
+    collectCoverageFrom: [
+        "utils/**/*.{ts,tsx}",
+        "services/**/*.{ts,tsx}",
+        "components/**/*.{ts,tsx}",
+        "context/**/*.{ts,tsx}",
+
+        "!**/*.d.ts",
+        "!**/node_modules/**",
+        "!**/assets/**",
+        "!**/constants/**",
+        "!**/types/**",
+        "!**/*index.{ts,tsx}",
+    ],
 };
